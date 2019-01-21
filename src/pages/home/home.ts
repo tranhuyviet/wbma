e5dc3f7ce4235595f0d1a4429d48af3e6a0a1d9e
Viet Tran <vietth@msn.com>
2019-01-21 13:28:09 +0200

    updated

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,7 +14,6 @@ import { MediaProvider } from '../../providers/media/media';
 export class HomePage implements OnInit {
 
   picArray: Pic[] = [];
-  picArray2: Pic[] = [];
   url = 'http://media.mw.metropolia.fi/wbma/uploads/';
 
   constructor(
@@ -30,13 +29,7 @@ export class HomePage implements OnInit {
 
   getPic() {
     this.mediaProvider.getAllMedia().subscribe((data: Pic[]) => {
-      /* console.log('data', data);
-      this.picArray = data.map((pic: Pic) => {
-        pic.thumbnails = this.url + pic.filename.substring(0, pic.filename.lastIndexOf('.')) + '-tn160.png';
-        console.log('pic after', pic);
-        return pic;
-      });*/
-      // B:
+
       data.forEach((pic: Pic) => {
         // add files to picArray
         this.mediaProvider.getSingleMedia(pic.file_id).subscribe((file: Pic) => {
